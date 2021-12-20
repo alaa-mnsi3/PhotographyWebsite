fb313e781885fb4c78116b4345b8573eccfebf8e
alaa-mnsi3 <alaamnsy334@gmail.com>
2021-12-21 00:56:14 +0200

    modified-project

--- dist/main.c52e0fe2.js
+++ dist/main.c52e0fe2.js
@@ -189,7 +189,7 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"./..\\..\\images\\xhome-banner.jpg.pagespeed.ic.mZauGRJbmf.webp":[["xhome-banner.jpg.pagespeed.ic.mZauGRJbmf.a84fc600.webp","../images/xhome-banner.jpg.pagespeed.ic.mZauGRJbmf.webp"],"../images/xhome-banner.jpg.pagespeed.ic.mZauGRJbmf.webp"],"_css_loader":"node_modules/parcel-bundler/src/builtins/css-loader.js"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"./..\\..\\images\\xhome-banner.jpg.pagespeed.ic.mZauGRJbmf.webp":[["xhome-banner.jpg.pagespeed.ic.mZauGRJbmf.a84fc600.webp","../images/xhome-banner.jpg.pagespeed.ic.mZauGRJbmf.webp"],"../images/xhome-banner.jpg.pagespeed.ic.mZauGRJbmf.webp"],"./..\\..\\images\\xbanner-2.jpg.pagespeed.ic.SLKsS03JdB.webp":[["xbanner-2.jpg.pagespeed.ic.SLKsS03JdB.975021eb.webp","../images/xbanner-2.jpg.pagespeed.ic.SLKsS03JdB.webp"],"../images/xbanner-2.jpg.pagespeed.ic.SLKsS03JdB.webp"],"_css_loader":"node_modules/parcel-bundler/src/builtins/css-loader.js"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63586" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52912" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
